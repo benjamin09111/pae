5e475ddacb5d4f8e062099463115f4d7351cc2df
nomadstar <ignacio.marambio_z@mail.udp.cl>
2024-02-14 12:25:24 -0300

    Actualización de variables de claves de Captcha en componente ReCaptcha

--- src/components/Captcha.jsx
+++ src/components/Captcha.jsx
@@ -1,11 +1,20 @@
 import ReCAPTCHA from "react-google-recaptcha";
 
 const ReCaptcha = ({setCaptchaOK}) => {
+  
+  const CaptchaKeys = {
+    siteKey: "6LcXtnApAAAAAM-OniROO8UbKM6jxkeSeI_dpIWq",
+    secretKey: "6LcXtnApAAAAACy05Mk3yjaUWslVu9WjdwY3UvlY"
+    //remplazar por claves propias
+  }
+  
+  
   const start = Date.now();
+
   const recaptchaChange = (value) => {
     setCaptchaOK(value ? true : false);
   };
-
+  
   const recaptchaExpired = () => {
     const end = Date.now();
     if (end - start < 60000) {
@@ -18,7 +27,7 @@ const ReCaptcha = ({setCaptchaOK}) => {
     <div className="recaptcha">
     <ReCAPTCHA
         onChange={recaptchaChange}
-        sitekey="6LcXtnApAAAAAM-OniROO8UbKM6jxkeSeI_dpIWq"
+        sitekey= {CaptchaKeys.siteKey}
         onExpired={recaptchaExpired}
         hl="es-419"
     />
