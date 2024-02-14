b0b7db3e8ab97ddef583ece91317bce1ffd480f4
nomadstar <ignacio.marambio_z@mail.udp.cl>
2024-02-14 12:12:38 -0300

    Añadida lógica para manejar la expiración de ReCaptcha

--- src/components/Captcha.jsx
+++ src/components/Captcha.jsx
@@ -1,15 +1,25 @@
 import ReCAPTCHA from "react-google-recaptcha";
 
 const ReCaptcha = ({setCaptchaOK}) => {
+  const start = Date.now();
   const recaptchaChange = (value) => {
     setCaptchaOK(value ? true : false);
   };
+
+  const recaptchaExpired = () => {
+    const end = Date.now();
+    if (end - start < 60000) {
+      setCaptchaOK(false);
+      return;
+    }
+  }
 
   return (
     <div className="recaptcha">
     <ReCAPTCHA
         onChange={recaptchaChange}
         sitekey="6LcXtnApAAAAAM-OniROO8UbKM6jxkeSeI_dpIWq"
+        onExpired={recaptchaExpired}
     />
 </div>
   )
